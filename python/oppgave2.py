947f93b9718711cc96c2537234050f520e3f8e52
Viktor Framnes <viktor.framnes@outlook.com>
2025-03-24 18:21:09 +0100

    fikset mappen med bare virussp programmene

--- python/oppgave2.py
+++
@@ -1,8 +0,0 @@
-tekst = "aaaassddaassffadaa"
-sum = 0
-
-for i in range(len(tekst)):
-    if tekst[i] == "s":
-        sum += 1
-
-print(f"Det er {sum} forekomster av bokstaven s")
--- python/oppgave4.py
+++
@@ -1,9 +0,0 @@
-s = int(input("Skriv inn sidelengden: "))
-
-for i in range(s):
-    if i >= 2 and i < s-2:
-        print("##", end="")
-        print(" "*(s-4), end="")
-        print("##")
-    else:
-        print("#"*s)
